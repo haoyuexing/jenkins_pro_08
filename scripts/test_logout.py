a5c2bc4a137871fbdfd425938c4230e66ff3db8f
haoyuexing <haoyuexing@gmail.com>
2020-10-31 15:57:42 +0800

    xxx

--- scripts/test_logout.py
+++ scripts/test_logout.py
@@ -5,3 +5,12 @@ class TestLogout:
     @pytest.mark.run(order=3)
     def test_logout4(self):
         print("logout4")
+
+    @pytest.mark.run(order=3)
+    def test_logout5(self):
+        print("logout4")
+
+
+    @pytest.mark.run(order=3)
+    def test_logout6(self):
+        print("logout4")
